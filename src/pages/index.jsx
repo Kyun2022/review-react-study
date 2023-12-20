faf0a37905f1bf7f17121420372b5dd5ed5acf03
kyun <fm13071303@rx.tnc.ne.jp>
2023-12-20 23:49:36 +0900

    ⚗️ 　不要な記事でいつか消しますが、試しにクリックイベントを記述しました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,17 @@
 // Components Imports
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useCallback } from "react";
 
 const Home = (props) => {
+  const handleClick = useCallback((e) => {
+    alert("kyun");
+  }, []);
+
   return (
     <div>
       <Header />
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" title="Index" />
     </div>
   );
