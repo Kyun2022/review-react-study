24c0fb0b2a5057d552f4024bb34eeb6295c1e013
kyun <fm13071303@rx.tnc.ne.jp>
2023-12-24 17:16:45 +0900

    ⚡️ 　useReducerを使用

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,36 +1,65 @@
 // Library Imports
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useReducer } from "react";
+
+const initialState = {
+  data: [],
+  loading: true,
+  error: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "end":
+      return {
+        ...state,
+        data: action.data,
+        loading: false,
+      };
+    case "error":
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+
+    default:
+      throw new Error("no such action type!");
+  }
+};
 
 export const Posts = () => {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const getPosts = useCallback(async () => {
     try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const res = await fetch("https://jsonplaceholder.typicode.com/postsa");
       if (!res.ok) {
         throw new Error("Failed to retrieve data because an error occurred");
       }
       const json = await res.json();
-      setPosts(json);
+      dispatch({
+        type: "end",
+        data: json,
+      });
     } catch (error) {
-      setError(error);
+      dispatch({
+        type: "error",
+        error,
+      });
     }
-    setLoading(false);
   }, []);
 
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  if (loading) {
+  if (state.loading) {
     return <div className="">Loading...</div>;
   }
-  if (error) {
-    return <div className="">{error.message}</div>;
+  if (state.error) {
+    return <div className="">{state.error.message}</div>;
   }
-  if (posts.length === 0) {
+  if (state.data.length === 0) {
     return <div className="">No data...</div>;
   }
 
@@ -38,7 +67,7 @@ export const Posts = () => {
     <>
       <div className="block">
         <ol className="list-decimal">
-          {posts.map((post) => {
+          {state.posts.map((post) => {
             return <li key={post.id}>{post.title}</li>;
           })}
         </ol>
